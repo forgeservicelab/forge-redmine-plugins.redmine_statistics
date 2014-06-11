c01c3b81be290f1c09f411f1dbeff0da81b9dc4f
Ilkka Koutonen <ilkka.koutonen@kainuunetu.fi>
2014-06-11 09:57:45 +0000

    Fixed SQL queries

--- erittainsalainenredminetiedosto7453295.php
+++ erittainsalainenredminetiedosto7453295.php
@@ -17,15 +17,15 @@ if (!mysql_select_db($database, $link)) {
 $name = mysql_real_escape_string($_GET['project_id']);
 
 //Calculates average from closed issues, but doesn't count issues that have been created as closed and welcome messages
-$average    = "select TIME_FORMAT(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Duration from issues INNER JOIN projects ON projec$
+$average    = "select TIME_FORMAT(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Duration from issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id=5 AND issues.updated_on != issues.created_on AND projects.identifier='".$name."' AND issues.subject!='Welcome to FORGE Service Lab!'";
 $average_result = mysql_query($average, $link);
 
 //Calculates max time from closed issues, but doesn't count issues that have been created as closed and welcome messages
-$max = "select TIME_FORMAT(SEC_TO_TIME(MAX(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Max from issues INNER JOIN projects ON projects.id = issu$
+$max = "select TIME_FORMAT(SEC_TO_TIME(MAX(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Max from issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id=5 AND projects.identifier='".$name."' AND issues.updated_on != issues.created_on AND issues.subject!='Welcome to FORGE Service Lab!'"
 $max_result = mysql_query($max, $link);
 
 //Calculates min time from closed issues, but doesn't count issues that have been created as closed and welcome messages
-$min = "select TIME_FORMAT(SEC_TO_TIME(MIN(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Min from issues INNER JOIN projects ON projects.id = issu$
+$min = "select TIME_FORMAT(SEC_TO_TIME(MIN(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Min from issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id=5 AND projects.identifier='".$name."' AND issues.updated_on != issues.created_on AND issues.subject!='Welcome to FORGE Service Lab!'";
 $min_result  = mysql_query($min, $link);
 
 //prints everything in tables
