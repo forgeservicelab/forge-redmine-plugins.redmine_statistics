f81d39956abaf1151cb0b2234859b0d4dc11a667
Jorge-Rodriguez <jorge.rodriguez@kampiki.com>
2014-08-19 15:02:21 +0300

    Moved timing queries into stats_controller. Removed iframe and PHP code.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,35 +1,48 @@
 class StatsController < ApplicationController
   unloadable
 
-before_filter :find_project, :authorize, :only => :index
+  before_filter :find_project, :authorize, :only => :index
 
   def index
 
-# Finds the project ID
-@project = Project.find_by_identifier(params[:project_id])
+    # Finds the project ID
+    @project = Project.find_by_identifier(params[:project_id])
 
-# Time counts for Issue amounts
-count_one = (Time.now - 1.day)..Time.now
-count_seven = (Time.now - 7.day)..Time.now
-count_fourteen = (Time.now - 14.day)..Time.now
-count_thirty = (Time.now - 30.day)..Time.now
+    # Time counts for Issue amounts
+    count_one = (Time.now - 1.day)..Time.now
+    count_seven = (Time.now - 7.day)..Time.now
+    count_fourteen = (Time.now - 14.day)..Time.now
+    count_thirty = (Time.now - 30.day)..Time.now
 
-# Counts the Issue amounts over certain time periods
-@total_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).to_a
-@one_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_one ).to_a
-@seven_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_seven ).to_a
-@fourteen_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_fourteen ).to_a
-@thirty_day_count =Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_thirty ).to_a
+    # Counts the Issue amounts over certain time periods
+    @total_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).to_a
+    @one_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_one ).to_a
+    @seven_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_seven ).to_a
+    @fourteen_day_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_fourteen ).to_a
+    @thirty_day_count =Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('created_on' => count_thirty ).to_a
 
-# Counts resolved and closed issues within the project
-@closed_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('status_id' => 5 ).to_a
-@resolved_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('status_id' => 3 ).to_a
+    # Counts resolved and closed issues within the project
+    @closed_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('status_id' => 5 ).to_a
+    @resolved_count = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).where('status_id' => 3 ).to_a
 
-@aika = Issue.joins(:project).where(projects: {identifier: params[:project_id]}).to_a
+    # Retrieves time stats for closed issues via custom SQL queries
+    adapter_type = Issue.connection.adapter_name.downcase.to_sym
+    case adapter_type
+      when :postgresql
+        @min_closure = Issue.connection.select_all("SET intervalstyle TO postgres_verbose; SELECT MIN(issues.updated_on - issues.created_on) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND issues.subject != 'Welcome to FORGE Service Lab!' AND projects.identifier = '#{params[:project_id]}'").to_a
+        @max_closure = Issue.connection.select_all("SET intervalstyle TO postgres_verbose; SELECT MAX(issues.updated_on - issues.created_on) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND issues.subject != 'Welcome to FORGE Service Lab!' AND projects.identifier = '#{params[:project_id]}'").to_a
+        @avg_closure = Issue.connection.select_all("SET intervalstyle TO postgres_verbose; SELECT AVG(issues.updated_on - issues.created_on) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND issues.subject != 'Welcome to FORGE Service Lab!' AND projects.identifier = '#{params[:project_id]}'").to_a
+      when :mysql || :mysql2
+        @min_closure = Issue.connection.select_all("SELECT TIME_FORMAT(SEC_TO_TIME(MIN(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND projects.identifier = '#{params[:project_id]}' AND issues.subject != 'Welcome to FORGE Service Lab!'").to_a
+        @max_closure = Issue.connection.select_all("SELECT TIME_FORMAT(SEC_TO_TIME(MAX(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND projects.identifier = '#{params[:project_id]}' AND issues.subject != 'Welcome to FORGE Service Lab!'").to_a
+        @avg_closure = Issue.connection.select_all("SELECT TIME_FORMAT(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(issues.updated_on,issues.created_on)))), '%H hours %i minutes %S seconds' ) AS Duration FROM issues INNER JOIN projects ON projects.id = issues.project_id WHERE issues.status_id = 5 AND issues.updated_on != issues.created_on AND projects.identifier = '#{params[:project_id]}' AND issues.subject != 'Welcome to FORGE Service Lab!'").to_a
+    else
+      raise NotImplementedError, "Unknown adapter type '#{adapter_type}'"
+    end
 
   end
 
-private
+  private
 
   def find_project
     # @project variable must be set before calling the authorize filter
